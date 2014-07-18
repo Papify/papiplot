64bb948e1bee24cd8f7ed4a0085690a8289636a9
panacotta <alejoar@gmail.com>
2014-07-18 20:31:55 +0200

    output files processing done.. now the ploting is what¡s left..
    Change-Id: Ia0d8fb98c65c5f482cd6326cc29245a60139d88c
    Signed-off-by: panacotta <alejoar@gmail.com>

--- papiplot/src/plot.c
+++ papiplot/src/plot.c
@@ -7,12 +7,30 @@
 
 #include "gnuplot_i.h"
 #include <dirent.h>
+typedef struct event_acumulator {
+	char* event_name;
+	unsigned long int count;
+}event_acumulator;
 
-typedef struct event_acum_ {
+typedef struct event_acum_for_action {
+	char* action_name;
 	int events_nb;
-	unsigned long int *acum;
-	char *event_name[];
-} event_acum_;
+	event_acumulator *acumulator[];
+}event_acum_for_action;
+
+typedef struct event_acum_for_actor {
+	char *proc_file_path;
+	char *actor_name;
+	int actions_nb;
+	event_acum_for_action *actions[];
+} event_acum_for_actor;
+
+event_acum_for_actor* process_data (char* path_todir, char* path_tofile, char* filename, char* path_to_totals);
+int get_number(char *line);
+void search_and_plot(char *path);
+void struct_test(struct event_acum_for_actor *data);
+int get_events_nb(char *path);
+int get_actions_nb_in_file(char* path_tofile);
 
 int main(int argc, char **argv) {
 
@@ -150,9 +168,18 @@ const int is_papioutput(const char *filename) {
 
 void search_and_plot(char *path) {
 
+	char * path_to_totals = malloc(strlen(path)+strlen("/plotdata_papi_totals.csv")+2);
+	strcpy(path_to_totals, path);
+	strcat(path_to_totals,"/plotdata_papi_totals.csv");
+	FILE* datafile = fopen(path_to_totals,"w");
+	fclose(datafile);
+
+	int actors_nb = get_nb_of_actors_in_dir(path);
+	event_acum_for_actor* data[actors_nb];
+
     struct dirent *pDirent;
     DIR *pDir;
-    int success = 0;
+
     pDir = opendir (path);
     if (pDir == NULL) {
         perror ("Cannot open directory\n");
@@ -160,72 +187,318 @@ void search_and_plot(char *path) {
     }
     char * path_to_csv;
 
+	int i = 0;
     while ((pDirent = readdir(pDir)) != NULL) {
-    	//usleep(30000);
     	if(is_papioutput(pDirent->d_name)) {
+    		printf("going for %s\n\n",pDirent->d_name);
     		path_to_csv = malloc(strlen(path)+strlen(pDirent->d_name)+2);
     		strcpy(path_to_csv, path);
     		strcat(path_to_csv,"/");
     		strcat(path_to_csv,pDirent->d_name);
-        	printf("plot of %s\n", pDirent->d_name);
+        	//printf("plot of %s\n", pDirent->d_name);
         	//plot(path_to_csv, pDirent->d_name);
-        	process_data(path, path_to_csv, pDirent->d_name);
-        	break;//temp..
+        	data[i] = process_data(path, path_to_csv, pDirent->d_name, path_to_totals);
+        	//struct_test(data[i++]);
         }
     }
-    printf("out of the loooop!\n");
+
+    //printf("out of the loooop!\n");
+    closedir (pDir);
     return;
 }
 
-void process_data (char* path_todir, char* path_tofile, char* filename){
+event_acum_for_actor* process_data (char* path_todir, char* path_tofile, char* filename, char* path_to_totals){
 	char buf[1500];
 	char *token;
-	int nb = 0;
-	char *saveptr;
-	event_acum_* acum;
-	FILE* ofile = fopen(path_tofile,"r");//original csv
+	event_acum_for_actor* actor;
 	char* path_todatafile = malloc(strlen(path_todir)+strlen("/papidata.temp")+4);
 	char* actor_name = &(*filename)+12;
-	printf("actor name is %s\n", actor_name);
+	//printf("actor name is %s\n", actor_name);
 	strcpy(path_todatafile, path_todir);
 	strcat(path_todatafile,"/");
 	strcat(path_todatafile,"plotdata_");
 	strcat(path_todatafile,actor_name);
-	printf("creating %s\n", path_todatafile);
-	FILE* datafile = fopen(path_todatafile,"w");
+	//printf("creating %s\n", path_todatafile);
 
-	int columns = get_events_nb(path_tofile)+2;
-	acum = malloc(sizeof(event_acum_)+sizeof(char*)*(columns-2));
-	acum->events_nb = get_events_nb(path_tofile);
-	acum->acum = malloc(sizeof(long int)*get_events_nb(path_tofile));
-	int i;
-	for(i = 0; i <acum->events_nb; i++){
-		acum->acum[i]=0;
+
+	int events_nb = get_events_nb(path_tofile);
+	//printf("we have %d actions here\n",get_actions_nb_in_file(path_tofile));/////////////////////////////////////////////
+	//get_actions_nb_in_file(path_tofile);
+	int actions_nb=get_actions_nb_in_file(path_tofile);
+
+	actor = malloc(sizeof(event_acum_for_actor)+sizeof(event_acum_for_action*)*(actions_nb));
+	actor->actor_name = malloc(strlen(actor_name));
+	strcpy(actor->actor_name,actor_name);
+	actor->proc_file_path = path_todatafile;
+	actor->actions_nb = actions_nb;
+	int i, j;
+
+	for(i=0;i<actions_nb;i++){
+		actor->actions[i] = malloc(sizeof(event_acum_for_action)+sizeof(event_acumulator)*events_nb);
+		actor->actions[i]->events_nb = events_nb;
+		//actor->actions[i]->action_name
+		for(j=0;j<events_nb;j++){
+			actor->actions[i]->acumulator[j] = malloc(sizeof(event_acumulator));
+			//actor->actions[i]->acumulator->event_name
+		}
 	}
 
-	fgets(buf,1500,ofile);//read first line
-	token = strtok(buf,";");
-	token = strtok(NULL,";");//skip action column
-	for(i=0;i<acum->events_nb;i++){
-		token = strtok(NULL,";");
-		acum->event_name[i] = malloc(strlen(token)+1);
-		strcpy(acum->event_name[i], token);
-		printf("%s saved\n", acum->event_name[i]);
-	}
-
-	//do{
-		fgets(buf,1500,ofile);
-		token = strtok(buf,";");
-		token = strtok(NULL,";");//skip actor name
-		token = strtok(NULL,";");//skip action name
-		for(i=0;i<acum->events_nb;i++){
-			acum->acum[i] += atol(strtok_r(token,"\"", &saveptr));
-			printf("evento %s, se suma: %s, total = %lu\n", acum->event_name[i], token, acum->acum[i]);
-			token = strtok(saveptr,";");
+	for(j = 0; j <actor->actions_nb; j++)
+		for(i = 0; i <actor->actions[j]->events_nb; i++){
+			actor->actions[j]->acumulator[i]->count = 0;
 		}
 
 
+	FILE* ofile = fopen(path_tofile, "r");
+	fgets(buf,1500,ofile);//read first line
+	token = strtok(buf,";");
+	token = strtok(NULL,";");//skip action column (actot name)
 
-	//}while(buf != NULL);
+	for(i=0;i<actor->actions[0]->events_nb;i++){
+		token = strtok(NULL,";");
+		for(j = 0; j <actor->actions_nb; j++){
+			actor->actions[j]->acumulator[i]->event_name = malloc(strlen(token)+1);
+			strcpy(actor->actions[j]->acumulator[i]->event_name, token);
+			//acum->acumulator->event_name[i] = malloc(strlen(token)+1);
+			//strcpy(acum->acumulator->event_name[i], token);
+		}
+	}
+
+	int found = -1;
+	int found_actions = 0;
+	int current;
+
+
+	long unsigned int totals[events_nb];
+	for (i=0;i<events_nb;i++){
+		totals[i]=0;
+	}
+
+	//acumuating structures
+	while(fgets(buf,1500,ofile)!=NULL){
+		token = strtok(buf,";");
+		token = strtok(NULL,";");//read action name
+
+		for(i=0; i<actor->actions_nb; i++){
+			//printf("comparing %s with %s\n", actor->actions[i]->action_name, token);
+			if(actor->actions[i]->action_name!=NULL && strcmp(actor->actions[i]->action_name,token)==0){
+				//printf("action %s already exist\n", actor->actions[i]->action_name);
+				found = i;
+				//printf("FOUND!\n");
+				break;
+			}
+		}
+		if (found == -1){
+			//printf("not found, adding action %s\n", token);
+			actor->actions[found_actions]->action_name = malloc(strlen(token)+1);
+			strcpy(actor->actions[found_actions]->action_name,token);
+		}
+
+		current = (found == -1)? found_actions++ : found;
+
+		for(i = 0; i <actor->actions[current]->events_nb; i++){
+			token = strtok(NULL,";");
+			actor->actions[current]->acumulator[i]->count +=  get_number(token);
+			totals[i] += get_number(token);
+		}
+
+		found = -1;
+	}
+
+	fclose(ofile);
+
+
+	//writing events per action
+	FILE* datafile = fopen(path_todatafile,"w");
+	fprintf(datafile,"Action;");
+	for(i=0;i< events_nb;i++)
+		fprintf(datafile,"%s;",actor->actions[0]->acumulator[0]->event_name);
+	fprintf(datafile,"\n");
+
+	for(i = 0; i< actions_nb;i++){
+		fprintf(datafile,"%s;", actor->actions[i]->action_name);
+		for(j=0; j < events_nb; j++){
+			fprintf(datafile,"%lu;", actor->actions[i]->acumulator[j]->count);
+		}
+		fprintf(datafile,"\n");
+	}
+	fclose(datafile);
+	//adding to totals per actor
+
+	int label_set = 0;
+	datafile = fopen(path_to_totals,"r");
+	fgets(buf,1500,datafile);
+	if(strstr(buf,"Actor;")!=NULL) label_set = 1;
+	fclose(datafile);
+
+	//appending to totals
+	datafile = fopen(path_to_totals,"a");
+	if(label_set == 0){
+		fprintf(datafile,"Actor;");
+		for(i=0;i< events_nb;i++)
+			fprintf(datafile,"%s;",actor->actions[0]->acumulator[0]->event_name);
+		fprintf(datafile,"\n");
+	}
+
+	fprintf(datafile,"%s;",strtok(actor->actor_name,"."));
+	for(j=0; j < events_nb; j++){
+		fprintf(datafile,"%lu;", totals[j]);
+	}
+	fprintf(datafile,"\n");
+	fclose(datafile);
+
+
+	return actor;
 }
 
+int get_number(char *line){ //removes quotes from string number
+	int lineLength = strlen(line);
+	int j = 0;
+	int i;
+	for (i = 0; i < lineLength; i++)
+	{
+	    if (line[i] == '\\')
+	    {
+	        line[j++] = line[i++];
+	        line[j++] = line[i];
+	        if (line[i] == '\0')
+	            break;
+	    }
+	    else if (line[i] != '"')
+	        line[j++] = line[i];
+	}
+	line[j] = '\0';
+
+	return atol(line);
+}
+
+//test structure
+void struct_test(struct event_acum_for_actor *data){
+	int i,j;
+	printf("Testing structure\n");
+
+	printf("Actor name: %s\n", data->actor_name);
+	printf("Path to processed data file: %s\n", data->proc_file_path);
+	printf("Actions number: %d\n", data->actions_nb);
+
+	for(i = 0; i<data->actions_nb;i++){
+		printf("\tAction name: %s\n", data->actions[i]->action_name);
+		printf("\tNumber of events: %d\n", data->actions[i]->events_nb);
+		for(j=0; j < data->actions[i]->events_nb; j++){
+			printf("\t\tEvent: %s, total: %lu\n", data->actions[i]->acumulator[j]->event_name, data->actions[i]->acumulator[j]->count);
+		}
+
+			//printf("Event: %s, total: %lu\n",data->, data->acumulator->acum[i]);
+	}
+}
+
+int get_nb_of_actors_in_dir(char* path){
+	int actors_nb=0;
+	struct dirent *pDirent;
+	DIR *pDir;
+
+	pDir = opendir (path);
+	if (pDir == NULL) {
+		perror ("Cannot open directory\n");
+		exit(1);
+	}
+
+	while ((pDirent = readdir(pDir)) != NULL) {
+		if(is_papioutput(pDirent->d_name)) {
+			actors_nb++;
+		}
+	}
+
+	closedir (pDir);
+
+	return actors_nb;
+}
+
+int get_actions_nb_in_file(char* path_tofile){//weird memory allocation... ¿??¿
+	int actions_nb = 0;
+	char** theActions;
+	char* token;
+	char buf[1500];
+	int i;
+	int found = -1;
+	theActions = malloc(0);
+
+	FILE* ofile = fopen(path_tofile, "r");
+
+	fgets(buf,1500,ofile);//skip labels lines
+	while(fgets(buf,1500,ofile)!=NULL){
+		token = strtok(buf,";");
+		token = strtok(NULL,";");//read action name
+
+		for(i=0; i<actions_nb; i++)
+			if(theActions[i]!=NULL && strcmp(theActions[i],token)==0){
+				found = i;
+				break;
+			}
+
+		if (found == -1){
+			theActions = realloc(theActions, sizeof(char*)*(actions_nb+2));
+			theActions[actions_nb] = malloc(strlen(token)+1);
+			theActions[actions_nb+1] = malloc(strlen(token)+1);
+			strcpy(theActions[actions_nb],token);
+			actions_nb++;
+		}
+		found = -1;
+	}
+
+	fclose(ofile);
+
+	for(i=0; i<actions_nb; i++)
+		free(theActions[i]);
+	free(theActions);
+
+	return actions_nb;
+}
+
+
+/*
+void get_nb_of_actions_in_actor(char* path, event_acum_per_action_* acum_per_action){
+	acum_per_action = malloc(sizeof(event_acum_per_action_));
+	acum_per_action->actions_nb = 0;
+	char* token;
+	char buf[1500];
+	FILE* ofile = fopen(path, "r");
+	int i, found = -1;
+	int result;
+
+	fgets(buf,1500,ofile);
+	while(fgets(buf,1500,ofile)!=NULL){
+		token = strtok(buf,";");
+		token = strtok(NULL,";");//skip actor name
+		//printf("found action %s\n", token);
+		if(token!=NULL){
+			for(i=0; i<acum_per_action->actions_nb; i++){
+				//printf("comparing %s(%d) with %s(%d)\n", acum_per_action->action_name[i], strlen(acum_per_action->action_name[i]), token, strlen(token));
+				if((result = strncmp(acum_per_action->action_name[i],token,strlen(token))==0)){
+					//printf("action %s already exist\n", acum_per_action->action_name[i]);
+					found = i;
+					break;
+				}
+			}
+			if (found == -1){
+				printf("not found, adding action %s\n", token);
+				acum_per_action->action_name[acum_per_action->actions_nb] = malloc(strlen(token)+1);
+				strcpy(acum_per_action->action_name[acum_per_action->actions_nb],token);
+
+				acum_per_action->actions_nb++;
+			}
+			else {
+				//already exists, sum acum on mark i
+				found = -1;
+			}
+		}
+	}
+	printf("struct test:\n");
+	printf("number of actions: %d\n", acum_per_action->actions_nb);
+	int j;
+	for(j = 0; j<=acum_per_action->actions_nb; j++){
+		printf("%s\n", acum_per_action->action_name[j]);
+	}
+
+	fclose(ofile);
+}*/
