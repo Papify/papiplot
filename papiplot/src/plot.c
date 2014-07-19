b3498ef7e276654b557df5b8fc5353b199489d7b
panacotta <alejoar@gmail.com>
2014-07-19 13:44:46 +0200

    sweet graphs
    Change-Id: I159334c009743a6b5a23bc413aded3fd19d627de
    Signed-off-by: panacotta <alejoar@gmail.com>

--- papiplot/src/plot.c
+++ papiplot/src/plot.c
@@ -52,9 +52,8 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
-void plot(char *path, char* actor_name) {
+void plot(char *path, char* actor_name, int number_of_actions, int number_of_events) {
 	int i;
-
 	gnuplot_ctrl * h ;
 	h = gnuplot_init() ;
 
@@ -63,13 +62,12 @@ void plot(char *path, char* actor_name) {
 	gnuplot_cmd(h,"add_label(d) = (strstrt(list, d) == 0 ? list=list.' '.d : '')");
 
 	gnuplot_cmd(h, "set datafile separator \";\"");
-	gnuplot_cmd(h, "set xtics rotate by -35");
-
-	int number_of_actions = 4; //TODO function that returns number of actions
-	int number_of_events = get_events_nb(path);
+	//gnuplot_cmd(h, "set xtics rotate by -35");
+	gnuplot_cmd(h, "set logscale y");
 
 	gnuplot_cmd(h, "set term png size 1680,1050");
-	gnuplot_cmd(h, "set output \"bar_labels.png\"");
+
+	gnuplot_cmd(h, "set output \"papiplot_%s.png\"",actor_name);
 	gnuplot_cmd(h, "set xlabel \"Actions\"");
 	gnuplot_cmd(h, "set ylabel \"Number of occurrences\"");
 
@@ -89,20 +87,14 @@ void plot(char *path, char* actor_name) {
 	gnuplot_cmd(h, "set xtics nomirror");
 	gnuplot_cmd(h, "set xtics autofreq");
 	gnuplot_cmd(h, "set xtics font \"Verdana,8\"");
-	gnuplot_cmd(h, "set label rotate by 90");
 	gnuplot_cmd(h, "set grid");
-
-	/*gnuplot_cmd(h, "set xtics(\\");
-	for (i=0;i<(number_of_actions-1);i++)
-		gnuplot_cmd(h,"\"\" %d,\\",i);
-	gnuplot_cmd(h,"\"\" %d)",i);*/
 
 
 	gnuplot_cmd(h, "plot \\");
 	if(number_of_events==1){
 		i = 0;
-		gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(2).'|', add_label(d),index(d))+(d_width*%d)):%d:xtic(2) smooth frequency w boxes title columnhead,\\", path, i, i+3);
-		//gnuplot_cmd(h, "\"%s\" using (d='|'.strcol(2).'|', add_label(d),index(d)):3:3 smooth frequency w labels rotate by 90 font \",5\" notitle", path);
+		gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(1).'|', add_label(d),index(d))+(d_width*%d)):%d:xtic(1) w boxes title columnhead,\\", path, i, i+2);
+		gnuplot_cmd(h, "\"%s\" using (d='|'.strcol(1).'|', add_label(d),index(d)):2:2 w labels rotate by 90 font \",5\" notitle", path);
 	}
 	else if (number_of_events==0){
 		printf("nothing to plot\n");
@@ -111,44 +103,21 @@ void plot(char *path, char* actor_name) {
 	else{
 		for(i=0;i<number_of_events;i++){
 			if(i == 0){//first
-				gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(2).'|', add_label(d),index(d))+(d_width*%d)):%d:xtic(2) smooth frequency w boxes title columnhead,\\", path, i, i+3);
-				//gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(2).'|', add_label(d),index(d))+(d_width*%d)):%d:%d smooth frequency w labels rotate by 90 font \",5\" notitle,\\", path,i,i+3,i+3);
-			}
+				gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(1).'|', add_label(d),index(d))+(d_width*%d)):%d:xtic(1) w boxes title columnhead,\\", path, i, i+2);
+				gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(1).'|', add_label(d),index(d))+(d_width*%d)):%d:%d w labels rotate by 90 font \",5\" notitle,\\", path, i, i+2, i+2);
+				}
 			else if (i==(number_of_events-1)){//last
-				gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(2).'|', add_label(d),index(d))+(d_width*%d)):%d smooth frequency w boxes title columnhead,\\", path, i, i+3);
-				//gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(2).'|', add_label(d),index(d))+d_width*%d):%d:%d smooth frequency w labels rotate by 90 font \",5\" notitle", path,i,i+3,i+3);
+				gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(1).'|', add_label(d),index(d))+(d_width*%d)):%d w boxes title columnhead,\\", path, i, i+2);
+				gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(1).'|', add_label(d),index(d))+(d_width*%d)):%d:%d w labels rotate by 90 font \",5\" notitle", path, i, i+2, i+2);
 			}
-			else
-				gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(2).'|', add_label(d)),index(d)+(d_width*%d)):%d smooth frequency w boxes title columnhead,\\", path, i, i+3);
-				//gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(2).'|', add_label(d),index(d))+(d_width*%d)):%d:%d smooth frequency w labels rotate by 90 font \",5\" notitle,\\", path,i,i+3,i+3);
+			else{
+				gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(1).'|', add_label(d),index(d))+(d_width*%d)):%d w boxes title columnhead,\\", path, i, i+2);
+				gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(1).'|', add_label(d),index(d))+(d_width*%d)):%d:%d w labels rotate by 90 font \",5\" notitle,\\", path, i, i+2, i+2);
+			}
 		}
 	}
-
+	//sleep(6);
 	gnuplot_close(h);
-
-
-//	gnuplot_cmd(h, "plot \\");
-
-//	gnuplot_cmd(h, "\"%s\" using (d='|'.strcol(2).'|', add_label(d),index(d)):3:xtic(2) smooth frequency w boxes title columnhead,\\", path);
-//	gnuplot_cmd(h, "\"%s\" using (d='|'.strcol(2).'|', add_label(d),index(d)):3:3 smooth frequency w labels rotate by 90 font \",5\" notitle,\\", path);
-
-//	gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(2).'|', add_label(d),index(d))+d_width):4 smooth frequency w boxes title columnhead,\\", path);
-//	gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(2).'|', add_label(d),index(d))+d_width):4:4 smooth frequency w labels rotate by 90 font \",5\" notitle,\\", path);
-
-//	gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(2).'|', add_label(d),index(d))+d_width*2):5 smooth frequency w boxes title columnhead,\\", path);
-//	gnuplot_cmd(h, "\"%s\" using ((d='|'.strcol(2).'|', add_label(d),index(d))+d_width*2):5:5 smooth frequency w labels rotate by 90 font \",5\" notitle", path);
-
-
-//	gnuplot_close(h);
-//	sleep(100);
-
-
-/*	plot "profit.dat" u 1:2 w boxes lc rgb"green" notitle,\
-	     "profit.dat" u ($1+d_width):3 w boxes lc rgb"red" notitle
-	     http://gnuplot-surprising.blogspot.com.es/2011/09/plot-histograms-using-boxes.html
-	     http://gnuplot-surprising.blogspot.com.es/2011/10/add-value-labels-to-top-of-bars-in-bar.html
-	     */
-	sleep(50000000);
 }
 
 int get_events_nb(char *path) {
@@ -196,24 +165,21 @@ void search_and_plot(char *path) {
     }
     char * path_to_csv;
 
-	int i = 0;
     while ((pDirent = readdir(pDir)) != NULL) {
     	if(is_papioutput(pDirent->d_name)) {
-    		printf("going for %s\n\n",pDirent->d_name);
     		path_to_csv = malloc(strlen(path)+strlen(pDirent->d_name)+2);
     		strcpy(path_to_csv, path);
     		strcat(path_to_csv,"/");
     		strcat(path_to_csv,pDirent->d_name);
         	//printf("plot of %s\n", pDirent->d_name);
         	data = process_data(path, path_to_csv, pDirent->d_name, path_to_totals);
-        	//plot(path_to_csv, pDirent->d_name);
+        	plot(data->proc_file_path, data->actor_name, data->actions_nb, data->actions[0]->events_nb);
+        	//todo generate html file with data
         	//remove temp files
-        	//free
-        	//struct_test(data[i++]);
+        	free(data);
         }
     }
 
-    //printf("out of the loooop!\n");
     closedir (pDir);
     return;
 }
@@ -317,8 +283,6 @@ event_acum_for_actor* process_data (char* path_todir, char* path_tofile, char* f
 	}
 
 	fclose(ofile);
-
-	struct_test(actor);
 
 	//writing events per action
 	FILE* datafile = fopen(path_todatafile,"w");
